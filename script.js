e6b7e916281840eeced2ef0464ae0b1eb0d3e98a
Kos0606 <65927936+Kos0606@users.noreply.github.com>
2021-02-15 22:02:19 +0000

    TV-show-dom-project

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function setup() {
   // level 100 
   allEpisodes.forEach(elem => {
     // let container = document.createElement('container');
-    let list = document.createElement('li');
+    let list = document.createElement('li'); 
     let title = document.createElement('p'); // display title
     let img = document.createElement('img'); // display image
     let summary = document.createElement('p'); // display summary
@@ -46,22 +46,11 @@ function setup() {
 }
 function myFunction() {
   // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById('mainContainer');
-  li = ul.getElementsByTagName('li');
+
+  
 
   // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
+  
 }
 
 function makePageForEpisodes(episodeList) {
@@ -70,4 +59,35 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+const mainContainer = document.getElementById('mainContainer');
+const searchBar = document.getElementById('searchBar');
+let allTvEpisodes = [];
+searchBar.addEventListener('keyup', (e) => {
+const searchString = e.target.value.toLowerCase();
+const filteredEpisodes = allTvEpisodes.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString)
+        );
+    });
+     displayEpisodes(filteredEpisodes);
+});
+
+const displayEpisodes = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li id ="list">
+                <h2>Title: ${elem.name} - S${pad(elem.season, 2)}E${pad(elem.number,2)}</h2>
+                <p>Summary: ${elem.summary}</p>
+                <img src= ${elem.image.medium}></img>
+            </li>
+        `;
+        })
+        .join('');
+    mainContainer.innerHTML = htmlString;
+};
+
+loadCharacters();
 
